e46568781145c6afe9eadb7da5e858dd229049a5
isabellemyz <isabellemyz33@gmail.com>
2024-06-11 10:29:56 -0700

    specified requirement.txt file is for khoa's system. cleaned up code in audio_input and fixed formatting of names

--- whisper_ai/audio_input.py
+++ whisper_ai/audio_input.py
@@ -8,15 +8,14 @@ from openai import OpenAI
 from dotenv import load_dotenv
 import os
 
-# Load variables from the .env file
+# loading variables from .env file
 load_dotenv()
-
-# Now you can access the variables using os.environ
 api_key = os.environ.get("OPENAI_API_KEY")
 
 client = OpenAI(api_key=api_key)
 
 def record_audio(output_filename, record_seconds=5, sample_rate=44100, chunk=1024):
+    # recording logic
     p = pyaudio.PyAudio()
 
     stream = p.open(format=pyaudio.paInt16,
@@ -39,27 +38,17 @@ def record_audio(output_filename, record_seconds=5, sample_rate=44100, chunk=102
     stream.close()
     p.terminate()
 
+    # if output file path DNE, then make it
+    directory = os.path.dirname(output_filename)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    # writing logic
     with wave.open(output_filename, 'wb') as wf:
         wf.setnchannels(1)
         wf.setsampwidth(p.get_sample_size(pyaudio.paInt16))
         wf.setframerate(sample_rate)
         wf.writeframes(b''.join(frames))
-
-# def transcribe_audio(file_path):
-#     client = OpenAI(api_key)
-
-#     audio_file = open(file_path, "rb")
-#     transcription = client.audio.transcriptions.create(
-#         model="whisper-1", 
-#         file=audio_file
-#     )
-#     print(transcription.text)
-
-
-# def transcribe_audio(file_path):
-#     model = whisper.load_model("base")
-#     result = model.transcribe(file_path)
-#     return result["text"]require
 
 def transcribe_audio(file_path):
 
@@ -69,13 +58,13 @@ def transcribe_audio(file_path):
     return transcription.text
 
 if __name__ == "__main__":
-    audio_file = "vocally-project/WhisperAI/Audio/test_audio.wav"
+    audio_file = "whisper_ai/audio_data/test_audio.wav"
 
     # Record audio from the microphone
     record_audio(audio_file, record_seconds=5)
 
     # Get the transcription of the recorded audio
-    transcription = transcribe_audio("vocally-project/WhisperAI/Audio/test_audio.wav")
+    transcription = transcribe_audio("whisper_ai/audio_data/test_audio.wav")
 
     # Print the transcription
     print("Transcription: ", transcription)
