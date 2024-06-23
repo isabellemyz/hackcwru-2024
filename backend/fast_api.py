247db232086193b17da4f51714a8749c96c7ce01
isabellemyz <isabellemyz33@gmail.com>
2024-06-22 18:11:33 -0700

    lots of refactoring - current endpoints work but are not connected to frontend

--- backend/fast_api.py
+++ backend/fast_api.py
@@ -1,20 +1,59 @@
 # UNTESTED AND UNEDITED CHATGPT CODE
 
 # app/main.py
-# from fastapi import FastAPI, File, UploadFile
-# from fastapi.middleware.cors import CORSMiddleware
-# import shutil
-# import os
+from fastapi import FastAPI, UploadFile, File, BackgroundTasks
+from fastapi.responses import JSONResponse, FileResponse
+from fastapi.middleware.cors import CORSMiddleware
 
-# app = FastAPI()
+from pydantic import BaseModel
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
 
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=["*"],  # Adjust this according to your requirements
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
+from processing.audio_input import record_audio
+from processing.audio_input import transcribe_audio
+from processing.model_input import get_response
+
+load_dotenv()
+api_key = os.environ.get("OPENAI_API_KEY")
+# client = OpenAI(api_key=api_key)
+
+class RecordAudioRequest(BaseModel):
+    filename: str
+    record_seconds: int
+    sample_rate: int
+    chunk: int
+
+class AudioOutput(BaseModel): 
+    filename: str
+
+
+class TextOutput(BaseModel):
+    text: str
+
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Adjust this according to your requirements
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.post("/record")
+async def record_audio_endpoint(request: RecordAudioRequest, background_tasks: BackgroundTasks):
+    background_tasks.add_task(record_audio, request.filename, request.record_seconds, request.sample_rate, request.chunk)
+    return JSONResponse(content={"message": "Recording started"})
+
+@app.post("/transcribe")
+async def transcribe_audio_endpoint(request: AudioOutput):
+    client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+    transcription = transcribe_audio(request.filename, client)
+    # os.remove("temp_audio.wav")
+    return JSONResponse(content={"transcription": transcription})
 
 # @app.post("/upload")
 # async def upload_file(file: UploadFile = File(...)):
@@ -27,6 +66,6 @@
 
 #     return {"filename": file.filename}
 
-# if __name__ == "__main__":
-#     import uvicorn
-#     uvicorn.run(app, host="0.0.0.0", port=8000)
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
